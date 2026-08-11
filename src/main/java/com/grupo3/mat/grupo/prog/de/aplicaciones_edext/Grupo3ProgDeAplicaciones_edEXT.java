622f4bf882251b00d0c00998973b32eff641e46b
Fede.Urtiberea <federico.urtiberea@estudiantes.utec.edu.uy>
2026-08-11 21:46:05 +0000

    Inicializacion del proyecto base edEXT

---
+++ src/main/java/com/grupo3/mat/grupo/prog/de/aplicaciones_edext/Grupo3ProgDeAplicaciones_edEXT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.grupo3.mat.grupo.prog.de.aplicaciones_edext;
+
+/**
+ *
+ * @author zeikof
+ */
+public class Grupo3ProgDeAplicaciones_edEXT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
